641939489cc1b26d96a2a0f7f4e2b0d60fd38d64
Ruhul-Mirza <ruhulmirza743@email.com>
2023-12-21 22:25:20 +0530

    Completed with Hooks

--- vite-project/src/Projects/SnakeLadder/Board.jsx
+++ vite-project/src/Projects/SnakeLadder/Board.jsx
@@ -1,36 +1,93 @@
-import React, { useEffect , useState } from "react";
-export default function Board({ position ,SNAKES ,LADDERS }) {
-  
-  const NUMBERS = [
-    [36, 35, 34, 33, 32, 31],
-    [25, 26, 27, 28, 29, 30],
-    [24, 23, 22, 21, 20, 19],
-    [13, 14, 15, 16, 17, 18],
-    [12, 11, 10, 9, 8, 7],
-    [1, 2, 3, 4, 5, 6],
-  ];
-  
+import React, { useEffect, useState } from "react";
+
+const NUMBERS = [
+  [36, 35, 34, 33, 32, 31],
+  [25, 26, 27, 28, 29, 30],
+  [24, 23, 22, 21, 20, 19],
+  [13, 14, 15, 16, 17, 18],
+  [12, 11, 10, 9, 8, 7],
+  [1, 2, 3, 4, 5, 6],
+];
+
+const SNAKES = {
+  8: 2,
+  16: 6,
+  23: 15,
+  28: 18,
+};
+const LADDERS = {
+  5: 10,
+  13: 19,
+  25: 33,
+};
+
+export default function Board({ dice, isGameActive }) {
+  const [position, setPosition] = useState(0);
+
+  useEffect(() => {
+    if (isGameActive) {
+      setPosition(1);
+    }
+  }, [isGameActive]);
+
+  useEffect(() => {
+    if (dice && isGameActive === true) {
+      setPosition(position + dice);
+    }
+  }, [dice]);
+
+  useEffect(() => {
+    if (position === 36) {
+      alert("Congratulations");
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }
+    if (position > 36) {
+      setPosition((prevposition) => prevposition - dice);
+    }
+    if (SNAKES[position]) {
+      setPosition(SNAKES[position]);
+      alert(
+        `You got bit by a snake! Descending from ${position} to ${SNAKES[position]}`
+      );
+    }
+    if (LADDERS[position]) {
+      setPosition(LADDERS[position]);
+      alert(
+        `You've found a ladder! Climbing from ${position} to ${LADDERS[position]}`
+      );
+    }
+  }, [position]);
 
   return (
     <>
-      <div className="mt-5 bg-white shadow-black shadow-md">
+      <div className="mt-5 bg-white shadow-black shadow-lg">
         {NUMBERS.map((row, rowIndex) => (
           <div key={rowIndex} className="flex">
             {row.map((number) => (
               <div
                 key={number}
-                className={`w-20 h-20 border border-emerald-900  text-emerald-900  flex items-center justify-center
-                ${ position === number ? "bg-emerald-900" : "bg-white"} 
-                ${position === number ? "text-white" : "text-emerald-900"}`}
+                className={`${position === number ? "border-8 bg-purple" : ""} 
+                            ${
+                              SNAKES[number]
+                                ? "bg-[url('./src/Projects/SnakeLadder/images/snake.png')] bg-cover"
+                                : ""
+                            } 
+                            ${
+                              LADDERS[number]
+                                ? "bg-[url('./src/Projects/SnakeLadder/images/ladder.png')] bg-center bg-cover"
+                                : ""
+                            } 
+                            w-20 h-20 border border-purple-900 flex items-center justify-center`}
               >
-                {number}
-                {LADDERS[number] && <span className="ladders">🚪</span>}
-                {SNAKES[number] && <span className="snakes">🐍</span>}
+                <span className="text-xl shadow-gray-700 drop-shadow-md font-extrabold text-center">
+                  {number}
+                </span>
               </div>
             ))}
           </div>
         ))}
-        
       </div>
     </>
   );
--- vite-project/src/Projects/SnakeLadder/Game.jsx
+++ vite-project/src/Projects/SnakeLadder/Game.jsx
@@ -1,83 +1,27 @@
-import RollDice from "../RollDice/RollDice";
-import { useState } from "react";
+import Dice from "./Dice";
+import { useState, useEffect } from "react";
 import Board from "./Board";
 
 export default function Game() {
   const [dice, setDice] = useState(1);
-  const [position, setPosition] = useState(0);
   const [isGameActive, setIsGameActive] = useState(false);
-
-  const SNAKES = {
-    8: 2,
-    16: 6,
-    23: 15,
-    28: 18,
-  };
-  const LADDERS = {
-    5: 10,
-    13: 19,
-    25: 33,
-  };
 
   const generateRandomNumber = () => {
     setDice(Math.floor(Math.random() * 6) + 1);
-    gameActive();
-  };
-
-  
-  const gameActive = () => {
-      if (dice === 6) {
-          setIsGameActive(true);
-        }
-    startGame();
-  };
-
-  const startGame = () => {
-      if (dice === 6 && !isGameActive) {
-          alert("let the game begins");
-        } else {
-            continueGame();
-        }
   };
   
-  const continueGame = () => {
-      if (isGameActive === true) {
-      setPosition(position + dice);
+  useEffect(() => {
+    if (dice === 6 && isGameActive === false) {
+      setIsGameActive(true);
+      alert("Game Begins");
     }
-    if (position <= 36) {
-        snakeAndLadder();
-    }
-    if (position === 36) {
-      endGame();
-    }
-};
-
-const snakeAndLadder = () => {
-    if (SNAKES[position]) {
-        const snakesEnd = SNAKES[position];
-        setPosition(SNAKES[position]);
-        alert(
-            `You got bit by a snake! Descending from ${position} to ${snakesEnd}`
-            );
-        }
-        if (LADDERS[position]) {
-            const laddersEnd = LADDERS[position];
-            alert(
-                `You've found a ladder! Climbing from ${position} to ${laddersEnd}`
-      );
-      setPosition(LADDERS[position]);
-    }
-};
-
-const endGame = () => {
-    alert("congratulation");
-};
+  }, [dice]);
 
   return (
     <>
-      <div className="flex flex-col items-center gap-6">
-        <Board position={position} SNAKES={SNAKES} LADDERS={LADDERS} />
-        <RollDice generateRandomNumber={generateRandomNumber} dice={dice} />
+      <div className="flex flex-col items-center gap-6 h-screen bg-purple-900">
+        <Board dice={dice} isGameActive={isGameActive} />
+        <Dice dice={dice} onClick={generateRandomNumber} />
       </div>
     </>
   );
